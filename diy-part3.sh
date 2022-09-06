3cfdada6a6df21a0f9d5f045c8adf844f6d82bb9
rainy888 <112684549+rainy888@users.noreply.github.com>
2022-09-06 09:21:26 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -27,7 +27,12 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #src-link custom /usr/src/openwrt/custom-feed
 #src-git helloworld https://github.com/fw876/helloworld
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git packages https://github.com/immortalwrt/packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/immortalwrt/luci' >>feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing' >>feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony' >>feeds.conf.default
+
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
